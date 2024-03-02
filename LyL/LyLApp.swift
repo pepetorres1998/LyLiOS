4485c4e1424c266f6ba8bf2514fe99f0784b8cda
Jose Antonio Torres Garibay <pepe_torres1998@hotmail.com>
2024-03-02 14:36:41 -0600

    Initial Commit

---
+++ LyL/LyLApp.swift
@@ -0,0 +1,17 @@
+//
+//  LyLApp.swift
+//  LyL
+//
+//  Created by Jose Anonio Torres Garibay on 02/03/24.
+//
+
+import SwiftUI
+
+@main
+struct LyLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
